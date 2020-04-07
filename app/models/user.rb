a0e4a4af79701b7479d257537c2c45356624c0b3
Konrad <kocasp@gmail.com>
2020-04-07 23:22:34 +0200

    Add registration validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :cases, foreign_key: "patient_id"
 
-  scope :doctor, -> { where(user_type: 'doctor') }
-  scope :patient, -> { where(user_type: 'patient') }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  PATIENT_TYPE = 'patient'
+  DOCTOR_TYPE = 'doctor'
+
+  scope :doctor, -> { where(user_type: DOCTOR_TYPE) }
+  scope :patient, -> { where(user_type: PATIENT_TYPE) }
 
   def active_case
     cases.where(status: 'active').take
@@ -16,5 +22,13 @@ class User < ApplicationRecord
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def doctor?
+    user_type == PATIENT_TYPE
+  end
+
+  def patient?
+    user_type == DOCTOR_TYPE
   end
 end
