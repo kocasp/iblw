458e06062fcd0dc4bf9eba07c4444380ad2153c7
Konrad <kocasp@gmail.com>
2020-04-07 15:26:35 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,10 @@
-# config/environments/production.rb
-config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+Rails.application.configure do
+  # config/environments/production.rb
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-if ENV["RAILS_LOG_TO_STDOUT"].present?
-  logger           = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
