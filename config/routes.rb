44ad0e8330ef1f1abbc00b1e94d461368949c2ba
Konrad <kocasp@gmail.com>
2020-04-08 21:06:51 +0200

    Tweak user STI solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  # devise_for :doctors, controllers: {
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations',
-  # }
-  # devise_for :patients, controllers: {
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations',
-  # }
-  devise_for :patients
-  devise_for :doctors
+  devise_for :doctors, controllers: {
+    # sessions: 'users/sessions',
+    registrations: 'doctors/registrations',
+  }
+  devise_for :patients, controllers: {
+    # sessions: 'users/sessions',
+    registrations: 'patients/registrations',
+  }
   resources :users
   resources :solutions
   resources :cases
