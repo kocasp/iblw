3548ce948c7100679804eb84e2b5c77fb5902802
Konrad <kocasp@gmail.com>
2020-04-23 22:07:36 +0200

    bootstrap-magnific

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,14 @@
 //= require theme/jquery.min
 //= require theme/popper.min
 //= require theme/bootstrap.min
+//= require theme/animated
+//= require theme/apexcharts
+//= require theme/charts
+//= require theme/chart-custom
+//= require theme/core
+//= require theme/countdown.min
+//= require theme/jquery.appear
+//= require theme/jquery.counterup.min
 //= require theme/jquery.magnific-popup.min
 //= require theme/select2.min
 //= require theme/custom
